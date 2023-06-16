01ed071353af7809576506fc1907fa54345bf5b9
Aguerra21 <84156826+Aguerra21@users.noreply.github.com>
2023-06-16 20:58:03 -0300

    LogIn Casi Terminado
    Conexion SQL hecha. Registro e Inicio de Sesion hecho.

---
+++ LogInDiseño/Form3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogInDiseño
+{
+    public partial class formMenu : Form
+    {
+        public formMenu()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void formMenu_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
