c28c7d0d206f04561dfdd332d753c194c3a8e22d
RenzoMacEachen <129195337+RenzoMacEachen@users.noreply.github.com>
2023-10-13 09:29:39 -0300

    Delete bdPro.sql

--- bdPro.sql
+++
@@ -1,8 +0,0 @@
-create database proyecto;
-use proyecto;
-
-create table usuarios (cedula int primary key, nombre varchar(50), apellido varchar(50) , email varchar(60), contraseña varchar(100));
-select * from usuarios;
-
-create table calendario (ID_tarea int primary key auto_increment, tarea varchar(50), fecha date);
-select * from calendario;
